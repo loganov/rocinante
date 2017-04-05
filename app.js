c9c37e30c0902e3b659210b450fc071dc240e735
quantumz <gregory.weaver@gmail.com>
2017-04-05 09:09:04 -0700

    Initial commit for nodejs hello world skeleton.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (request, response) {
+  response.send('Wrong URL! Try /hello.')
+})
+
+app.get('/hello', function (request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Sample application for ganymede.');
+  console.log('Listening on port 3000');
+})
+
+process.on('SIGINT', function() {
+  console.log('Exiting...');
+  process.exit();
+});
